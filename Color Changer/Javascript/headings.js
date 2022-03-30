d747cad0c5ca44020cf0219e372f18834f672ca9
Mujaid <82204967+Mujaid156@users.noreply.github.com>
2022-03-30 10:14:37 +0200

    Add files via upload

--- Color Changer/Javascript/headings.js
+++ Color Changer/Javascript/headings.js
@@ -12,14 +12,96 @@ function text() {
   }
   
 function HeadFirst(event) {
-    var p = document.querySelector(".head");
-    if (p) {
+    var p = document.getElementById("main-header");
+    var h3 = document.getElementById("main-header");
+
+    if (p && h3 ) {
       p.style.color = event.target.value;
+      h3.style.color = event.target.value
     }
   }
-  function headAll(event) {
-    document.querySelectorAll(".head").forEach(function(p) {
+  function HeadAll(event) {
+    document.getElementById("main-header").forEach(function(p) {
         console.log(p, event.target.value)
       p.style.color = event.target.value;
     });
   }
+
+//   document.getElementById("main-header").style.color = "blue";
+
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');
+          img.onload = () => {
+              URL.revokeObjectURL(img.src);  // no longer needed, free memory
+          }
+
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      }
+  });
+});
+
+var dragItem = document.querySelector("#item");
+    var container = document.querySelector("#container");
+
+    var active = false;
+    var currentX;
+    var currentY;
+    var initialX;
+    var initialY;
+    var xOffset = 0;
+    var yOffset = 0;
+
+    container.addEventListener("touchstart", dragStart, false);
+    container.addEventListener("touchend", dragEnd, false);
+    container.addEventListener("touchmove", drag, false);
+
+    container.addEventListener("mousedown", dragStart, false);
+    container.addEventListener("mouseup", dragEnd, false);
+    container.addEventListener("mousemove", drag, false);
+
+    function dragStart(e) {
+      if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX - xOffset;
+        initialY = e.touches[0].clientY - yOffset;
+      } else {
+        initialX = e.clientX - xOffset;
+        initialY = e.clientY - yOffset;
+      }
+
+      if (e.target === dragItem) {
+        active = true;
+      }
+    }
+
+    function dragEnd(e) {
+      initialX = currentX;
+      initialY = currentY;
+
+      active = false;
+    }
+
+    function drag(e) {
+      if (active) {
+      
+        e.preventDefault();
+      
+        if (e.type === "touchmove") {
+          currentX = e.touches[0].clientX - initialX;
+          currentY = e.touches[0].clientY - initialY;
+        } else {
+          currentX = e.clientX - initialX;
+          currentY = e.clientY - initialY;
+        }
+
+        xOffset = currentX;
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, dragItem);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    }
--- Color Changer/index.php
+++ Color Changer/index.php
@@ -1,19 +1,49 @@
-<?php
 
-/**
- * Plugin Name:       Color Switcher
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
- * Author:            John Smith
- */
+ <head>
+  <meta name="viewport" 
+  content="width=device-width, 
+  initial-scale=1.0, 
+  user-scalable=no" />
+   <link rel="stylesheet" href="styles.css">
+ </head>
+ <body class="body">
 
-echo '<html>';
-echo '<head>';
-echo '</head>';
-echo '<body class="body">';
-echo '<label for="colorpicker">Color Picker:</label>';
-echo '<input type="color" id="colorpicker" value="#0000ff">';
-echo '<script src="/script.js"></script>';
-echo '</body>';
-echo '</html>';
-?>
+  <label for="colorpicker">Color Picker:</label>
+  <input type="color" id="colorpicker" value="#0000ff">
+
+   <h1 id="main-header">This is a header</h1>
+
+   <label for="colorHead">Color Picker:</label>
+   <input type="color" id="colorHead" value="#ff0000">
+
+   <h3 class="sub">Sub header</h3>
+
+   <label for="colorSub">Color Picker:</label>
+   <input type="color" id="colorSub" value="#ff0000">
+
+   <p class="color">Watch the paragraph colors change when you adjust the color picker.
+    As you make changes in the color picker, the first paragraph's
+    color changes, as a preview (this uses the <code>input</code>
+    event). When you close the color picker, the <code>change</code>
+    event fires, and we detect that to change every paragraph to
+    the selected color.</p>
+
+    <label for="colorWell">Color Picker:</label>
+    <input type="color" id="colorWell" value="#ff0000">
+
+    <div id="outerContainer">
+      <div id="container">
+        <div id="item">
+          
+        </div>
+      </div>
+    </div>
+
+    <script src="./Javascript/scriptbody.js"></script>
+   <script src="./Javascript/script.js"></script>
+   <script src="./Javascript/headings.js"></script>
+   <script src="./Javascript/sub.js"></script>
+ </body>
+
+
+<?php 
